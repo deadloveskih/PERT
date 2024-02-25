12c4405a231bead1f3ded727d7e8211e74bbbe43
deadloveskih <stepandedlovskih@gmail.com>
2024-02-25 13:11:35 +0500

    Create help function

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ from commands import *
 from work_with_data import init_file
 
 
+def help() -> None:
+    print("MIT License Copyright (c) 2024 deadloveskih", end="\n\n")
+    print("add command")
+    print("\tusing: add 'task_name' (1) (2) (3)", end="\n\n")
+    print("\t(1) float optimistic value of time for task")
+    print("\t(2) float nominal value of time for task")
+    print("\t(3) float pessimistic value of time for task", end="\n\n")
+    print("\tadd record in csv files", end="\n\n")
+    print("del command")
+    print("\tusing: del 'task_name'", end="\n\n")
+    print("\tdelete record from csv files", end="\n\n")
+    print("print command")
+    print("\tusing: print 'task_name'", end="\n\n")
+    print("\tprint values of one task", end="\n\n")
+    print("show command")
+    print("\tusing: show", end="\n\n")
+    print("\tprint values of all tasks", end="\n\n")
+    print("summarize command")
+    print("\tusing: summarize", end="\n\n")
+    print("\tprint summarize time and deviation of all tasks", end="\n\n")
+
+
 def main() -> None:
     assert init_file("data/input_data.csv", ["Task", "Optimistic", "Nominal", "Pessimistic"])
     assert init_file("data/output_data.csv", ["Task", "Time", "Deviation"])
@@ -22,7 +44,7 @@ def main() -> None:
             case ["summarize"]:
                 summarize()
             case ["help"]:
-                break
+                help()
             case _:
                 print(f"Undefined command: {user_input}")
                 
