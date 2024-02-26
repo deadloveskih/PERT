c13d6076cc37568caae84fe2242e87a113b6bbdb
deadloveskih <stepandedlovskih@gmail.com>
2024-02-26 20:19:38 +0500

    Optimize adding task to cache

--- commands/pert_add.py
+++ commands/pert_add.py
@@ -1,6 +1,5 @@
 from work_with_data import *
 from calculations import *
-from .pert_print import print_data
 
 def add_data(task_name: str, optimistic: str, nominal: str, pessimistic: str) -> dict:
     if add_input_data(task_name, optimistic, nominal, pessimistic):
@@ -8,4 +7,11 @@ def add_data(task_name: str, optimistic: str, nominal: str, pessimistic: str) ->
         deviation = calc_deviation(float(optimistic), float(pessimistic))
         add_output_data(task_name, str(time), str(deviation))
 
-    return print_data(task_name)
+        return {
+                    "Deviation": deviation,
+                    "Nominal": nominal,
+                    "Optimistic": optimistic,
+                    "Pessimistic": pessimistic,
+                    "Task": task_name,
+                    "Time": time
+                }
